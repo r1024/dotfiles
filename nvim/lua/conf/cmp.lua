51a397c1b0ce7f7dbc165f32f5ab34f854171114
r1024 <wenchen_mails@163.com>
2022-06-26 07:03:01 +0800

    nvim config support zsh luasnip

--- nvim/lua/conf/cmp.lua
+++ nvim/lua/conf/cmp.lua
@@ -2,6 +2,7 @@ local lspkind = require('lspkind')
 local cmp = require('cmp')
 
 require("luasnip.loaders.from_vscode").lazy_load()
+require'luasnip'.filetype_extend("zsh", {"shell"})
 
 cmp.setup({
   snippet = {
